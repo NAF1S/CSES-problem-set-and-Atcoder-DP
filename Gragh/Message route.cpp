d66085b21ec27e60ea84e74d0814390795d5cf03
nafis <lokmansharif000@gmail.com>
2025-05-17 23:39:59 +0600

    c

--- Gragh/Message route.cpp
+++ Gragh/Message route.cpp
@@ -25,5 +25,48 @@ const ll dy[] = {1, 0, -1, 0};
 
 
 int main(){
+    int n,m;cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int x,y;cin>>x>>y;
+        adj[x].pb(y);
+        adj[y].pb(x);
+    }
+    vector<int>from(100005,0);
+
+    queue<int>q;
+    q.push(1);
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        vis[u]=1;
+        if(u == n){
+            break;
+        }
+        for(int x:adj[u]){
+            if(vis[x]==-1){
+                vis[x]=1;
+                from[x]=u;
+                q.push(x);
+            }
+        }
+    }
+    vector<int>path;
+    if(vis[n]==-1){
+        cout<<"IMPOSSIBLE"<<nl;
+    }
+    else{
+        path.pb(n);
+        int curr = n;
+        while(curr != 1){
+            curr = from[curr];
+            path.pb(curr);
+        }
+        cout<<path.size()<<nl;
+        reverse(path.begin(),path.end());
+        for(int x:path){
+            cout<<x<<" ";
+        }
+    }
+
     
 }
